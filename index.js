7692065c3f1ebf76a43962f046c4d774efd67fac
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2024-05-08 11:29:21 +0530

    dd

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import connectdb from './db/index.model.js';
 import { todomodel } from './models/todo.model.js';
+import path from 'path'
 const app = express()
+app.use(urlencoded({extended:"false"}))
 app.set('view engine','ejs')
-
+app.set("views", path.resolve("./views"))
 app.get('/',(req,res)=>{
-    res.render('./views/index')
+    res.render('index')
+})
+app.post('/',(req,res)=>{
+const text = req.body;
+console.log(text.text)
+
 })
 connectdb().then(()=>{
     app.listen('2000',()=>{
